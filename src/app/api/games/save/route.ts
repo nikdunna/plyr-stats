08dcfa8774d155c39148fc368020e6a33fbb0638
Nik Dunna <nik.dunna@gmail.com>
2025-04-21 23:08:40 -0400

    PLEASE

--- src/app/api/games/save/route.ts
+++ src/app/api/games/save/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { prisma } from "@/config/prisma";
+import { authOptions } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 
 export async function POST(request: Request) {
--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/config/utils";
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
